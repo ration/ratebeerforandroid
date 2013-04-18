1272e9d053046dbe657016a6bcc0e8269779bd30
Eric Kok <erickok@gmail.com>
2013-04-18 08:03:05 +0200

    Moved Ndef caode to NfcHelper class to prevent crash on loading beer fragment.

--- RateBeerForAndroid/src/com/ratebeer/android/gui/components/helpers/NfcHelper.java
+++ RateBeerForAndroid/src/com/ratebeer/android/gui/components/helpers/NfcHelper.java
@@ -1,7 +1,9 @@
 package com.ratebeer.android.gui.components.helpers;
 
 import android.annotation.TargetApi;
+import android.net.Uri;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateNdefMessageCallback;
 import android.nfc.NfcEvent;
@@ -40,10 +42,14 @@ public class NfcHelper {
 		public NdefMessage createNdefMessage(NfcEvent event) {
 			// A device is close; ask the currently visible fragment to create a message
 			Fragment content = activity.getSupportFragmentManager().findFragmentById(R.id.frag_content);
-			if (content != null && content instanceof CreateNdefMessageCallback)
-				return ((CreateNdefMessageCallback) content).createNdefMessage(event);
+			if (content != null && content instanceof NdefUriProvider)
+				return new NdefMessage(new NdefRecord[] { NdefRecord.createUri(((NdefUriProvider) content)
+						.createNdefUri()) });
 			return null;
 		}
 	};
 
+	public interface NdefUriProvider {
+		public Uri createNdefUri();
+	}
 }
